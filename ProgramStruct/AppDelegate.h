e56af7cf3e1f1b227701ef9fe258b7f83256d9fe
神一样的电脑二 <benden@xbpeng-macbookpro-2.local>
2016-12-14 12:02:25 +0800

    Initial Commit

---
+++ ProgramStruct/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProgramStruct
+//
+//  Created by 神一样的电脑二 on 2016/12/14.
+//  Copyright © 2016年 神一样的电脑二. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProgramStruct/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ProgramStruct
+//
+//  Created by 神一样的电脑二 on 2016/12/14.
+//  Copyright © 2016年 神一样的电脑二. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
